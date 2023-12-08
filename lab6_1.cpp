909a257b2cf6c4cec03a8a5a0bb6ad896e734a38
Verawit <Verawit2548@gmail.com>
2023-12-08 15:58:18 +0700

    Filled out README

--- lab6_1.cpp
+++ lab6_1.cpp
@@ -2,8 +2,20 @@
 using namespace std;
 
 int main(){
-    cout << "Enter an integer: ";
-    cout << "#Even numbers = ";
-    cout << "#Odd numbers = ";
+    int num;
+    int even = 0;
+    int odd = 0;
+    while (num != 0) {
+        cout << "Enter an integer: " << endl;
+        cin >> num;
+        if (num%2 == 0){
+            even++;
+        }else{
+            odd++;
+        }
+    }
+
+    cout << "#Even numbers = " << even << endl;
+    cout << "#Odd numbers = " << odd << endl;
     return 0;
 }
